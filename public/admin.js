fd038e03cf0706144d967f4602b0b2e7381fdc54
root <root@ip-172-31-92-247.ec2.internal>
2019-11-16 02:06:38 +0000

    test

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,27 @@
 var app = new Vue({
   el: '#admin',
-  data: {},
-  methods: {}
+  data: {
+    title: "",
+    file: null,
+    addItem: null,
+  },
+  methods: {
+  fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/items', {
+          title: this.title,
+          path: r1.data.path
+        });
+        this.addItem = r2.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
 });
--- server.js
+++ server.js
@@ -17,4 +17,54 @@ mongoose.connect('mongodb://localhost:27017/museum', {
 });
 
 
+// Configure multer so that it will upload to '/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: './public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  path: String,
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
 app.listen(3000, () => console.log('Server listening on port 3000!'));
+
+
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+  const item = new Item({
+    //console.log("items creating new item in the museum!");
+    title: req.body.title,
+    path: req.body.path,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
+
+// Upload a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    //console.log("Uploading photo, return the path of stored photo!");
+    path: "/images/" + req.file.filename
+  });
+});
